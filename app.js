385ab0841c6a395130a996b098635adf74195173
mahm0udnasr <mahmoudnasrlite@gmail.com>
2024-07-07 08:11:56 +0200

    Add New Products &  Search

--- app.js
+++ app.js
@@ -1,82 +1,3 @@
-
-const products = [
-  {
-    id: 1,
-    title: "Air Force",
-    price: 119,
-    colors: [
-      {
-        code: "black",
-        img: "./img/air.png",
-      },
-      {
-        code: "darkblue",
-        img: "./img/air2.png",
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: "Air Jordan",
-    price: 149,
-    colors: [
-      {
-        code: "lightgray",
-        img: "./img/jordan.png",
-      },
-      {
-        code: "green",
-        img: "./img/jordan2.png",
-      },
-    ],
-  },
-  {
-    id: 3,
-    title: "Blazer",
-    price: 109,
-    colors: [
-      {
-        code: "lightgray",
-        img: "./img/blazer.png",
-      },
-      {
-        code: "green",
-        img: "./img/blazer2.png",
-      },
-    ],
-  },
-  {
-    id: 4,
-    title: "Crater",
-    price: 129,
-    colors: [
-      {
-        code: "black",
-        img: "./img/crater.png",
-      },
-      {
-        code: "lightgray",
-        img: "./img/crater2.png",
-      },
-    ],
-  },
-  {
-    id: 5,
-    title: "Hippie",
-    price: 99,
-    colors: [
-      {
-        code: "gray",
-        img: "./img/hippie.png",
-      },
-      {
-        code: "black",
-        img: "./img/hippie2.png",
-      },
-    ],
-  },
-];
-
 const sliderWrapper = document.querySelector('.sliderWrapper');
 const slides = document.querySelectorAll('.sliderItem');
 const prevButton = document.querySelector('.prev');
@@ -101,3 +22,19 @@ const prevSlide = () => {
 prevButton.addEventListener('click', prevSlide);
 nextButton.addEventListener('click', nextSlide);
 setInterval(nextSlide, 3000);
+
+
+// search
+document.getElementById('searchInput').addEventListener('input', function() {
+    let filter = this.value;
+    let product = document.querySelectorAll('.product');
+    for (let i = 0; i < product.length; i++) {
+        let title = product[i].querySelectorAll('.productTitle')[0];
+        if (title.innerHTML.indexOf(filter) > -1) {
+            product[i].style.display = '';
+        } else {
+            product[i].style.display = 'none';
+        }
+    }
+});
+
