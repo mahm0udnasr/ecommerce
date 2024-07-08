7cf44485728d81c019ca8c349592d267a2f5fb83
mahm0udnasr <mahmoudnasrlite@gmail.com>
2024-07-08 06:19:46 +0200

    develop the shoping cart

--- app.js
+++ app.js
@@ -53,3 +53,68 @@ openCart.onclick = ()=> {
         shopCart.style.display = 'none';
     }
 }
+
+/**
+    *Shopping Cart
+        * get data from proudct 
+        * send data for localstorage 
+        * show data in shopping cart 
+        * on click delete icon  - delete the product from local storage
+        * get sum in totalPrice text
+ */
+
+
+const addToCartButtons = document.querySelectorAll('.addCart');
+addToCartButtons.forEach(button => {
+    button.addEventListener('click', addToCart);
+});
+loadCart();
+
+
+function addToCart(event) {
+    const productDiv = event.target.parentElement;
+    console.log(productDiv);
+    const productName = productDiv.querySelector('.productTitle').textContent;
+    const productPrice = parseFloat(productDiv.querySelector('.productPrice').textContent);
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    cart.push({ name: productName, price: productPrice });
+    localStorage.setItem('cart', JSON.stringify(cart));
+    displayCart();
+}
+
+function loadCart() {
+    displayCart();
+}
+
+function displayCart() {
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+    const savedCart = document.getElementById('savedCart')
+    savedCart.innerHTML = '';
+    let totalPrice = 0;
+    cart.forEach((item, index) => {
+        let ProudctDetails = `
+            <div class="saveCart">
+                <div class="savepImg">
+                    <img class="productImg" loading="lazy" alt="Nike Air Max" width="60" height="60" src="https://via.placeholder.com/60">
+                </div>
+                <div class="savepDetails">
+                    <div class="saveTitle">${item.name}</div>
+                    <div class="savePrice">$${item.price}</div>
+                </div>
+                <div class="saveDeleted" id="deleteProduct" onclick="removeFromCart(${index})">
+                    <i class="fa-solid fa-trash"></i>
+                </div>
+            </div>
+        `;
+        savedCart.innerHTML += ProudctDetails
+        totalPrice += item.price;
+    });
+    document.querySelector(".total").textContent = totalPrice.toFixed(2);
+}
+
+function removeFromCart(index) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    cart.splice(index, 1);
+    localStorage.setItem('cart', JSON.stringify(cart));
+    displayCart();
+}
